9f674d2ae0ea267dbe119b8d0ac71ba36bea5737
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 14:38:55 -0500

    Add project files.

---
+++ C#_Currency_Temp_Converter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Currency_Temp_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
