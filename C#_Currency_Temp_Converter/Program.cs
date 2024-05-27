63997c1491595041a2aef53216f3a403915d5833
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 14:56:46 -0500

    Completed logic for Currency Converter

--- C#_Currency_Temp_Converter/Program.cs
+++ C#_Currency_Temp_Converter/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace C__Currency_Temp_Converter
+﻿//Following this tutorial => https://youtu.be/CigGiIb7HeM?si=BfAMw9x91ojlxioV
+
+namespace C__Currency_Temp_Converter
 {
-    internal class Program
+    //Remove internal from the class name
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Currency and Temperature Converter!");
+            Console.WriteLine("Please enter...\n\nc) Currency Converter\nanything else) Temperature Converter");
+            string answer = Console.ReadLine();
+
+            if (answer == "c")
+            {
+                Console.Clear();
+                Console.WriteLine(" -- Currency Converter -- ");
+                Console.WriteLine("Please enter...\n\na) Convert Pounds\nanything else) Convert Euros\n\n");
+                string choice = Console.ReadLine();
+
+                if (choice == "a")
+                {
+                    Console.Clear();
+                    Console.Write("Please enter your amount in British Pounds: ");
+                    float pounds = float.Parse(Console.ReadLine());
+                    float euros = pounds * 1.16f;
+                    Console.WriteLine($"{pounds} British Pounds converted to Euros is {euros.ToString("0.00")}");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Please enter your amount in Euros: ");
+                    float euros = float.Parse(Console.ReadLine());
+                    float pounds = euros / 1.16f;
+                    Console.WriteLine($"{euros} Euros converted to British Pounds is {pounds.ToString("0.00")}");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(" -- Temperature Converter -- ");
+            }
         }
     }
 }
